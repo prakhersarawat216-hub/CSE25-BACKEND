1559bb63dbdc2f3d821b947323c7a5bc40a9f28d
prakhersarawat216-hub <prakhersaraswat216@gmail.com>
2026-07-27 16:29:21 +0530

    day3

--- day3.js
+++ day3.js
@@ -1,14 +1,31 @@
-//*****************Asynchronous js using asynch/await*************/////
-async function test(){
-    console.log("2:message");
+// //*****************Asynchronous js using asynch/await*************/////
+// async function test(){
+//     console.log("2:message");
+//     await console.log("3:message");
+//     console.log("4:message");
+
+// }
+// console.log("1:message")
+// test()
+// console.log("5:message") 
+//create promises that resolve with Assignment submitted
+// and print using then(), reject with submission falied
+//async function that display user data 
+//class practice question:async function that display user data
+//async function for data json data fetch
+async function data() {
+    await console.log("2:message");
+    const response=fetch("./students.json");
+    console.log(response.status);
+    const std=await response.json();
+    return std;
     await console.log("3:message");
     console.log("4:message");
-
 }
 console.log("1:message")
-test()
-console.log("5:message") 
-
-
-//create promises that resolve with Assignment submitted  */
-// and print using then(), reject with submission falied
+data()
+    .then((std) => {
+        console.log(res);
+    }).catch((err) => {
+        console.log(error);
+    });
